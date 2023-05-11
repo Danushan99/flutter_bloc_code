409498eb535b62d9d5d9218b44dc27ba5cd01916
Danushan Ravendran <it20051648@my.sliit.lk>
2023-05-11 09:51:09 +0530

    ADD

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -16,54 +16,33 @@ class _WelcomeState extends State<Welcome> {
       color: Colors.white,
       child: Scaffold(
         body: Container(
+          margin: EdgeInsets.only(top: 34.h),
           width: 370.w,
           child: Stack(
             children: [
               PageView(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.w,
-                        child: Text("image 1"),
-                      ),
-                      Container(
-                        child: Text("First See Learning",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 24.sp,
-                                fontWeight: FontWeight.normal)),
-                      ),
-                      Container(
-                        width: 375.w,
-                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
-                        child: Text(
-                            "its all about learning the  knowladgeits all about learning the knowladge ",
-                            style: TextStyle(
-                                color: Colors.black.withOpacity(0.5),
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.normal)),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                            top: 100.w, right: 25.w, left: 25.w),
-                        width: 325.w,
-                        height: 50.w,
-                        decoration: BoxDecoration(
-                            color: Colors.redAccent,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15.w)),
-                            boxShadow: [
-                              BoxShadow(color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 1,
-                                blurRadius: 2,
-                                offset: Offset(10, 10)
-                              ),
-                            ]),
-                      )
-                    ],
-                  )
+                  _page(
+                      1,
+                      context,
+                      "Next",
+                      "First See Learning",
+                      "its all about learning the  knowladgeits all about learning",
+                      "imagepath"),
+                  _page(
+                      2,
+                      context,
+                      "Next",
+                      "Connect with Everyone",
+                      "its all about learning the  knowladgeits all about learning",
+                      "imagepath"),
+                  _page(
+                      3,
+                      context,
+                      "Get Start",
+                      "Free for Everyone ",
+                      "its all about learning the  knowladgeits all about learning",
+                      "imagepath"),
                 ],
               )
             ],
@@ -72,4 +51,56 @@ class _WelcomeState extends State<Welcome> {
       ),
     );
   }
+}
+
+Widget _page(int Index, BuildContext context, String buttonName, String title,
+    String subTitle, String imagePath) {
+  return Column(
+    children: [
+      SizedBox(
+        width: 345.w,
+        height: 345.w,
+        child: Text(imagePath),
+      ),
+      Container(
+        child: Text(title,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 24.sp,
+                fontWeight: FontWeight.normal)),
+      ),
+      Container(
+        width: 375.w,
+        padding: EdgeInsets.only(left: 30.w, right: 30.w),
+        child: Text(subTitle,
+            style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontSize: 14.sp,
+                fontWeight: FontWeight.normal)),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 100.w, right: 25.w, left: 25.w),
+        width: 325.w,
+        height: 50.w,
+        decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(Radius.circular(15.w)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: Offset(0, 1)),
+            ]),
+        child: Center(
+            child: Text(
+          buttonName,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.normal),
+        )),
+      )
+    ],
+  );
 }
