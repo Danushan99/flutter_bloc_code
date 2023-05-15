2061e3aff9b544142010137e4ab3613449d3a8e4
Danushan Ravendran <it20051648@my.sliit.lk>
2023-05-16 00:56:46 +0530

    add guster detctor

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -95,7 +95,7 @@ Widget _page(int Index, BuildContext context, String buttonName, String title,
       SizedBox(
         width: 345.w,
         height: 345.w,
-        child: Image.asset(imagePath)
+        child: Image.asset(imagePath, fit: BoxFit.cover,)
       ),
       Container(
         child: Text(title,
@@ -113,28 +113,31 @@ Widget _page(int Index, BuildContext context, String buttonName, String title,
                 fontSize: 14.sp,
                 fontWeight: FontWeight.normal)),
       ),
-      Container(
-        margin: EdgeInsets.only(top: 100.w, right: 25.w, left: 25.w),
-        width: 325.w,
-        height: 50.w,
-        decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(Radius.circular(15.w)),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 1,
-                  blurRadius: 2,
-                  offset: Offset(0, 1)),
-            ]),
-        child: Center(
-            child: Text(
-          buttonName,
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 16.sp,
-              fontWeight: FontWeight.normal),
-        )),
+      GestureDetector(
+        onTap: (){},
+        child: Container(
+          margin: EdgeInsets.only(top: 100.w, right: 25.w, left: 25.w),
+          width: 325.w,
+          height: 50.w,
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(Radius.circular(15.w)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: Offset(0, 1)),
+              ]),
+          child: Center(
+              child: Text(
+            buttonName,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.normal),
+          )),
+        ),
       )
     ],
   );
